439b817d2fb15e320c27312bfdcab25aa1fa6637
proshanta <proshant.cse@gmail.com>
2015-10-11 22:58:59 +0530

    Adding Script for Histogram

--- Scripts/AnalysisScript.R
+++ Scripts/AnalysisScript.R
@@ -1,4 +1,4 @@
-  setwd("C:\\Users\\IBM_ADMIN\\Desktop\\MatricsGrimoire\\PreAnalysisQuery\\Scripts")
+  setwd("C:\\Users\\IBM_ADMIN\\Desktop\\MatricsGrimoire\\PreAnalysisQuery\\Grimoire\\Metricsgrimoire-analysis\\Scripts")
 
 #importing all supporting custom methods
   source("Methods.R")
@@ -9,8 +9,12 @@
   rs<-executeQuery(conn,"select repository_id, count(distinct author_id) number_of_unique_author from scmlog group by repository_id order by number_of_unique_author DESC")
   MSR_ECLIPSE_SOURCE_CODE<- fetch(rs, n = -1)
   write.table(x=MSR_ECLIPSE_SOURCE_CODE,"..\\Output\\MSR_ECLIPSE_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
-  
+  pdf("..\\Output\\MSR_ECLIPSE_SOURCE_CODE_HIST.pdf")
+  hist(MSR_ECLIPSE_SOURCE_CODE$number_of_unique_author)
+  dev.off()
   closeConnection(conn)
+  
+  
 
 #Creating MySql connection for MSR_OPENSTACK_SOURCE_CODE
   conn<-mySqlConnection(dbName="MSR_OPENSTACK_SOURCE_CODE")
@@ -18,7 +22,9 @@
   rs<-executeQuery(conn,"select repository_id, count(distinct author_id) number_of_unique_author from scmlog group by repository_id order by number_of_unique_author DESC")
   MSR_OPENSTACK_SOURCE_CODE<- fetch(rs, n = -1)
   write.table(x=MSR_OPENSTACK_SOURCE_CODE,"..\\Output\\MSR_OPENSTACK_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
-  
+  pdf("..\\Output\\MSR_OPENSTACK_SOURCE_CODE_HIST.pdf")
+  hist(MSR_OPENSTACK_SOURCE_CODE$number_of_unique_author)  
+  dev.off()
   closeConnection(conn)
   
   
