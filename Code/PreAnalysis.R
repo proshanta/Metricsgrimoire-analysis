c6ed6f008d0f4a8ac2f73bc0a50c0df11ac8e639
proshanta <proshant.cse@gmail.com>
2015-12-28 19:58:15 +0530

    Commit for CCN

--- Code/PreAnalysis.R
+++ Code/PreAnalysis.R
@@ -1,4 +1,4 @@
-  setwd("C:\\Users\\IBM_ADMIN\\Desktop\\MatricsGrimoire\\PreAnalysisQuery\\Grimoire\\Metricsgrimoire-analysis\\Scripts")
+  setwd("C:/Users/IBM_ADMIN/Desktop/Subhajit Sir/Grimoire/Metricsgrimoire-analysis/Code")
 
 #importing all supporting custom methods
   source("Methods.R")
@@ -8,15 +8,15 @@
 
   rs<-executeQuery(conn,"select repository_id, count(distinct author_id) number_of_unique_author from scmlog group by repository_id order by number_of_unique_author DESC")
   MSR_ECLIPSE_SOURCE_CODE<- fetch(rs, n = -1)
-  write.table(x=MSR_ECLIPSE_SOURCE_CODE,"..\\Output\\MSR_ECLIPSE_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
-  pdf("..\\Output\\MSR_ECLIPSE_SOURCE_CODE_HIST.pdf")
+  write.table(x=MSR_ECLIPSE_SOURCE_CODE,"..\\Output\\PreAnalysis\\MSR_ECLIPSE_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
+  pdf("..\\Output\\PreAnalysis\\MSR_ECLIPSE_SOURCE_CODE_HIST.pdf")
   hist(MSR_ECLIPSE_SOURCE_CODE$number_of_unique_author)
   dev.off()
   #Summary Descriptive Statistics for eclipse
   print("Summary Descriptive Statistics for eclipse")
   library(psych)
   e<-describe(MSR_ECLIPSE_SOURCE_CODE$number_of_unique_author)
-  write.table(x=e,"..\\Output\\Stat_eclipse.csv",sep = ",")
+  write.table(x=e,"..\\Output\\PreAnalysis\\Stat_eclipse.csv",sep = ",")
   closeConnection(conn)
   
   
@@ -26,14 +26,14 @@
   
   rs<-executeQuery(conn,"select repository_id, count(distinct author_id) number_of_unique_author from scmlog group by repository_id order by number_of_unique_author DESC")
   MSR_OPENSTACK_SOURCE_CODE<- fetch(rs, n = -1)
-  write.table(x=MSR_OPENSTACK_SOURCE_CODE,"..\\Output\\MSR_OPENSTACK_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
-  pdf("..\\Output\\MSR_OPENSTACK_SOURCE_CODE_HIST.pdf")
+  write.table(x=MSR_OPENSTACK_SOURCE_CODE,"..\\Output\\PreAnalysis\\MSR_OPENSTACK_SOURCE_CODE.csv",sep = ",",row.names = FALSE)
+  pdf("..\\Output\\PreAnalysis\\MSR_OPENSTACK_SOURCE_CODE_HIST.pdf")
   hist(MSR_OPENSTACK_SOURCE_CODE$number_of_unique_author)  
   dev.off()
   #Summary Descriptive Statistics for Openstack
   print("Summary Descriptive Statistics for openstack")
   d<-describe(MSR_ECLIPSE_SOURCE_CODE$number_of_unique_author)
-  write.table(x=d,"..\\Output\\Stat_openstack.csv",sep = ",")
+  write.table(x=d,"..\\Output\\PreAnalysis\\Stat_openstack.csv",sep = ",")
   closeConnection(conn)
   
   
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Metricsgrimoire-analysis
-To analysze the metricsgrimoire data
+##Code co-change network (CCN)
+1. You will find the code in code folder with the name as CCN.r.
+2. The script takes a csv as input which you can find in Analysis folder with the name CCNrepoList.csv.
+3. The result networks you will find in CCN folder which is a sub folder in Output and the network name is as it's repo name.
+
